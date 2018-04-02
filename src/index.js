d01435c5ebfe86d0b4a984ad1774e3b6956d6658
Hareendra Seneviratne <hareendras@millenniumitesp.com>
2018-04-02 17:41:17 +0530

    Setup React Router

--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import { Router, Route, IndexRoute, BrowserHistory } from "react-router";
 
-import App from './components/app';
-import reducers from './reducers';
+import App from "./components/app";
+import reducers from "./reducers";
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+    <Router history={BrowserHistory}>
+      <Route path="/" component={App} />
+    </Router>
+  </Provider>,
+  document.querySelector(".container")
+);
