c9c226eeaa574db7ef9f8ef7ffd73ac5925fa29e
Hareendra Seneviratne <hareendras@millenniumitesp.com>
2018-04-03 11:18:31 +0530

    Lec 91

--- src/index.js
+++ src/index.js
@@ -2,17 +2,20 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware } from "redux";
-import { Router, Route, IndexRoute, BrowserHistory } from "react-router";
+import { Router, Route, IndexRoute, browserHistory } from "react-router";
 
-import App from "./components/app";
+import app from "./components/app";
 import reducers from "./reducers";
+import signin from "./components/auth/signin";
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
-    <Router history={BrowserHistory}>
-      <Route path="/" component={App} />
+    <Router history={browserHistory}>
+      <Route path="/" component={app}>
+        <Route path="signin" component={signin} />
+      </Route>
     </Router>
   </Provider>,
   document.querySelector(".container")
